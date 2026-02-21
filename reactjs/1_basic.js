369316b3dbd973cce06637c266e2da5e8612a82e
janvi moradiya <moradiyajanvi1112@gmail.com>
2026-02-21 18:33:52 +0530

    Add files via upload

---
+++ reactjs/1_basic.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div>
+  <h1>Welcome to the reactjs</h1>
+</div>
+  
+);
+
+
