de8c1149f578811f5a4bd63b029d70033f164e3a
janvi moradiya <moradiyajanvi1112@gmail.com>
2026-02-22 19:35:21 +0530

    Add files via upload

---
+++ reactjs/6_decision_making_statement.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './clock.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function DigitalClock(datetime) {
+    
+    let { day, month, year, hour, minute, seconds } = datetime;
+    let ampm = null;
+    if (hour > 12) {
+        hour = hour - 12;
+        ampm = "PM";
+    }
+    else {
+        ampm = "AM";
+    }
+    return (
+        <>
+            <div className="matrix-bg"></div>
+            <div className="container-fluid clock-container d-flex justify-content-center align-items-center">
+                <div className="text-center glow-box">
+                    <div id="clock" className="clock">{hour}:{minute}:{seconds} {ampm}</div>
+                    <div id="date" className="date mt-3">Saturday {day}-{month}-{year} </div>
+                </div>
+            </div>
+        </>
+    )
+}
+let d = new Date(); 
+let datetime = {
+    day: d.getDate(),
+    month: d.getMonth() + 1,
+    year: d.getFullYear(),
+    hour: d.getHours(),
+    minute: d.getMinutes(),
+    seconds: d.getSeconds()
+}
+root.render(DigitalClock(datetime))
